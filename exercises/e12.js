087fcbd39ee75784d37473fa54fcdc93601098b5
Derek <derekpayne1991@hotmail.com>
2024-07-10 12:21:32 -0400

    Solved e12

--- exercises/e12.js
+++ exercises/e12.js
@@ -1,15 +1,22 @@
-
 // EXERCISE 12
 // Return an array of all deposits greater than 100
 // Array example: bankAccounts in /data/data.js
 // getAllDepositsGreaterThanOneHundred(bankAccounts) => [3432, 43242.34, 23432]
 
 export function getAllDepositsGreaterThanOneHundred(array) {
-  // Your code goes here...
-
+	// Your code goes here...
+	const depositsOverOneHundred = [];
+	for (let i = 0; i < array.length; i++) {
+		if (array[i].deposits) {
+			for (let j = 0; j < array[i].deposits.length; j++) {
+				if (array[i].deposits[j] > 100) {
+					depositsOverOneHundred.push(array[i].deposits[j]);
+				}
+			}
+		}
+	}
+	return depositsOverOneHundred;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-12"
