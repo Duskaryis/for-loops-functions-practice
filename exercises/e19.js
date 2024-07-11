998703e972d1ab40c3870d587ff93c67a704a53e
Derek <derekpayne1991@hotmail.com>
2024-07-11 11:14:55 -0400

    Solved e19

--- exercises/e19.js
+++ exercises/e19.js
@@ -1,15 +1,22 @@
-
 // EXERCISE 19
 // Please, read the exercise-info/reverse.md to get the initial data of what is the expected result of this exercise.
 // reverseArray([1, 2, 3 ,4]) => [4, 3, 2, 1]
 // NOTE: You can NOT use the array.reverse() method in your code
 
 export function reverseArray(array) {
-  // Your code goes here...
+	// Your code goes here...
+	let left = 0;
+	let right = array.length - 1;
 
+	while (left < right) {
+		let temp = array[left];
+		array[left] = array[right];
+		array[right] = temp;
+		left++;
+		right--;
+	}
+	return array;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-19"
